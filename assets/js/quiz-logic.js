787ff7cda2aee9c5dc1639e5f3e04a3a3d483bfe
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-10-01 18:41:43 -0700

    question div becomes visible, replacing start screen when game begins

--- assets/js/quiz-logic.js
+++ assets/js/quiz-logic.js
@@ -1,13 +1,19 @@
 console.log(questions)
 // only start screen visible at first
-// upon click of start button, start screen hidden, question div becomes visible
 
 var startBtnEl = document.getElementById('start-btn');
 
+//Initialize question index to 0
+
+// upon click of start button, start screen hidden, 
 function startGame(){
     var startScreenEL = document.querySelector('.start')
     startScreenEL.setAttribute("style", "display: none")
     startScreenEL.style.visibility = 'hidden';
+
+    // question div becomes visible
+    var questionEL = document.querySelector(".question");
+    questionEL.removeAttribute("style");
 }
 
 startBtnEl.onclick = startGame;
