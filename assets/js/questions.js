4d2f2bbfe381fb9c92ba7e12c899c1fd1da010fb
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-10-02 19:12:11 -0700

    refactor logic script function definitions. Add question to question array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,5 +3,10 @@ var questions = [
         question: "Which of the following sorting algorithms involves splitting an array into halves and sorting each half first?",
         choices: ['Selection Sort', 'Merge Sort', 'Bubble Sort'],
         answer: "Merge Sort"
+    },
+    {
+        question: "Which sorting algorithm works by comparing adjacent numbers and moving the largest values towards the end of the array?",
+        choices: ["Bubble Sort", "Selection Sort", "Quick Sort", "Merge Sort"],
+        answer: "Bubble Sort"
     }
 ]
--- assets/js/quiz-logic.js
+++ assets/js/quiz-logic.js
@@ -1,19 +1,42 @@
-console.log(questions)
 // only start screen visible at first
+var startBtnEl = document.getElementById("start-btn");
 
-var startBtnEl = document.getElementById('start-btn');
+// upon click of start button, start screen hidden,
+const startGame = function(){
+  var startScreenEL = document.querySelector(".start");
+  startScreenEL.setAttribute("style", "display: none");
+  startScreenEL.style.visibility = "hidden";
 
-//Initialize question index to 0
-
-// upon click of start button, start screen hidden, 
-function startGame(){
-    var startScreenEL = document.querySelector('.start')
-    startScreenEL.setAttribute("style", "display: none")
-    startScreenEL.style.visibility = 'hidden';
-
-    // question div becomes visible
-    var questionEL = document.querySelector(".question");
-    questionEL.removeAttribute("style");
+  // question div becomes visible
+  var questionDiv = document.querySelector(".question");
+  questionDiv.removeAttribute("style");
+  showQuestion();
 }
 
+const showQuestion = function () {
+  // Initialize questionIndex to 0
+  var questionIndex = 0;
+  console.log(questions);
+
+  // set the question title text content to title of question[questionIndex]
+  var titleEl = document.querySelector(".question-title");
+  titleEl.textContent = questions[questionIndex].question;
+
+  var choicesDiv = document.querySelector('.choices');
+  // create a button for each of question[questionIndex]'s choices, 
+  for (let i = 0; i < questions[questionIndex].choices.length; i++) {
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = questions[questionIndex].choices[i];
+    // append to choices div
+    choicesDiv.append(choiceButton);
+  }
+}
+
+// check answer function
+const checkAnswer = function(){
+
+}
+
+
 startBtnEl.onclick = startGame;
+ 
